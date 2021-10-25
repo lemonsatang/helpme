9d4b3a5a8acbe67aa375c0326e42aa6880afb6bb
lemonsatang <73567157+lemonsatang@users.noreply.github.com>
2021-10-25 15:44:57 +0900

    dfsd
    dfds

--- project_helpme/kj_add.php
+++ project_helpme/kj_add.php
@@ -2,21 +2,30 @@
 
 	include 'database.php';
 
-	$comp = $_POST['COMP'];
-	$c_code = $_POST['C_CODE'];
 	$j_bunho = $_POST['J_BUNHO'];
+	$kj_ilja = $_POST['KJ_ILJA'];
+	$c_code = $_POST['C_CODE'];
+	$comp = $_POST['COMP'];
+	$projno = $_POST['PROJNO'];
+	$soosin = $_POST['SOOSIN'];
+	$chamjo = $_POST['CHAMJO'];
 	$phone = $_POST['PHONE'];
 	$fax = $_POST['FAX'];
 	$usrnm = $_POST['USRNM'];
 	$i_jogun = $_POST['I_JOGUN'];
 	$j_jogun = $_POST['J_JOGUN'];
+	$nabgi = $_POST['NABGI'];
+	$yoohyo = $_POST['YOOHYO'];
 	$gunsoo = $_POST['GUNSOO'];
 	$sryang = $_POST['SRYANG'];
 	$jryang = $_POST['JRYANG'];
+	$gonhggup = $_POST['GONGGUP'];
+	$booga = $_POST['BOOGA'];
+	$hapgye = $_POST['HAPGYE'];
 	$bigo = $_POST['BIGO'];
 	$cdate = date("Y-m-d", time());
 	$cuser = $_POST['CUSER'];
 	$temp = $_POST['TEMP'];
 
-	$link->query("INSERT INTO kj_m(COMP,C_CODE,J_BUNHO,PHONE,FAX,USRNM,I_JOGUN,J_JOGUN,GUNSOO,SRYANG,JRYANG,BIGO,CDATE,CUSER,TEMP)
-	VALUES('".$comp."','".$c_code."','".$j_bunho."','".$phone."','".$fax."','".$usrnm."','".$i_jogun."','".$j_jogun."','".$gunsoo."','".$sryang."','".$jryang."','".$bigo."','".$cdate."','".$cuser."','".$temp."')");
+	$link->query("INSERT INTO kj_m(J_BUNHO,KJ_ILJA,C_CODE,COMP,PROJNO,SOOSIN,CHAMJO,PHONE,FAX,USRNM,I_JOGUN,J_JOGUN,NABGI,YOOHYO,GUNSOO,SRYANG,JRYANG,GONGGUP,BOOGA,HAPGYE,BIGO,CDATE,CUSER,TEMP)
+	VALUES('".$j_bunho."','".$kj_ilja."','".$c_code."','".$comp."','".$projno."','".$soosin."','".$chamjo."','".$phone."','".$fax."','".$usrnm."','".$i_jogun."','".$j_jogun."','".$nabgi."','".$yoohyo."','".$gunsoo."','".$sryang."','".$jryang."','".$gonhggup."','".$booga."','".$hapgye."','".$bigo."','".$cdate."','".$cuser."','".$temp."')");

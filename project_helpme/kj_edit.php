a739de53d3727531a1e47dc26e3f72ea015314f3
lemonsatang <73567157+lemonsatang@users.noreply.github.com>
2021-10-25 15:48:23 +0900

    SDSDDSSDFSDSFD
    SFDSFDSFDSDFFD

--- project_helpme/kj_edit.php
+++ project_helpme/kj_edit.php
@@ -3,34 +3,52 @@
 	include 'database.php';
 
 	$id = $_POST['ID'];
-	$comp = $_POST['COMP'];
-	$c_code = $_POST['C_CODE'];
 	$j_bunho = $_POST['J_BUNHO'];
+	$kj_ilja = $_POST['KJ_ILJA'];
+	$c_code = $_POST['C_CODE'];
+	$comp = $_POST['COMP'];
+	$projno = $_POST['PROJNO'];
+	$soosin = $_POST['SOOSIN'];
+	$chamjo = $_POST['CHAMJO'];
 	$phone = $_POST['PHONE'];
 	$fax = $_POST['FAX'];
 	$usrnm = $_POST['USRNM'];
 	$i_jogun = $_POST['I_JOGUN'];
 	$j_jogun = $_POST['J_JOGUN'];
+	$nabgi = $_POST['NABGI'];
+	$yoohyo = $_POST['YOOHYO'];
 	$gunsoo = $_POST['GUNSOO'];
 	$sryang = $_POST['SRYANG'];
 	$jryang = $_POST['JRYANG'];
+	$gonhggup = $_POST['GONGGUP'];
+	$booga = $_POST['BOOGA'];
+	$hapgye = $_POST['HAPGYE'];
 	$bigo = $_POST['BIGO'];
 	$mdate = date("Y-m-d", time());
 	$muser = $_POST['MUSER'];
 	$temp = $_POST['TEMP'];
 
 	$link->query("UPDATE kj_m 
-					SET COMP = '".$comp."'
-					, C_CODE = '".$c_code."'
-					, J_BUNHO = '".$j_bunho."'
+					SET , J_BUNHO = '".$j_bunho."'
+					, COMP = '".$comp."'
+					, KJ_ILJA = '".$kj_ilja."'
+					, COMP = '".$comp."'
+					, PROJNO = '".$projno."'
+					, SOOSIN = '".$soosin."'
+					, CHAMJO = '".$chamjo."'
 					, PHONE = '".$phone."'
 					, FAX = '".$fax."'
 					, USRNM = '".$usrnm."'
 					, I_JOGUN = '".$i_jogun."'
 					, J_JOGUN = '".$j_jogun."'
+					, NABGI = '".$nabgi."'
+					, YOOHYO = '".$yoohyo."'
 					, GUNSOO = '".$gunsoo."'
 					, SRYANG = '".$sryang."'
 					, JRYANG = '".$jryang."'
+					, GONGGUP = '".$gonhggup."'
+					, BOOGA = '".$booga."'
+					, HAPGYE = '".$hapgye."'
 					, BIGO = '".$bigo."'
 					, mdate = '".$mdate."' 
 					, muser = '".$muser."' 
